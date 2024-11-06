09e1f1bb90f509ce08221337e94f8a0e33833354
zewa-bamuga <tikhonov.igor2028@yandex.ru>
2024-11-06 20:03:26 +0700

    Now notifications for News come 24 hours in advance

--- src/app/domain/users/core/tasks.py
+++ src/app/domain/users/core/tasks.py
@@ -1,3 +1,4 @@
+import asyncio
 from typing import Any
 
 from dependency_injector import wiring
@@ -7,6 +8,11 @@ from app.domain.common.enums import TaskNames
 from app.domain.common.schemas import IdContainer
 from app.domain.users.core.commands import UserActivateCommand, EmailSenderCommand
 from a8t_tools.bus.consumer import consume
+
+from datetime import datetime, timezone, timedelta
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 @consume(TaskNames.activate_user)
@@ -19,18 +25,31 @@ async def activate_user(
     await activate_user(user_id_container.id)
 
 
+user_timezone = timezone(timedelta(hours=7))
+
+
 @consume(TaskNames.reminder_news)
 @wiring.inject
 async def reminder_news(
         reminder_id_container_dict: dict[str, Any],
         reminder_news: EmailSenderCommand = wiring.Provide[Container.user.email_sender_command],
-        execution_time: str | None = None,  # добавьте этот параметр
+        execution_time: str | None = None
 ) -> None:
     reminder_id_container = IdContainer.model_validate(reminder_id_container_dict)
 
-    # Вы можете использовать execution_time здесь, если это необходимо
     if execution_time:
-        # Логика обработки времени, если нужно
-        print(f"Напоминание будет отправлено в {execution_time}")
+        # Преобразуем `execution_time` в нужный часовой пояс
+        scheduled_time = datetime.fromisoformat(execution_time).astimezone(user_timezone)
+        current_time = datetime.now(user_timezone)
 
+        delay = (scheduled_time - current_time).total_seconds()
+
+        # Логируем запланированное время отправки в вашем часовом поясе
+        logger.info(f"Уведомление запланировано на отправку в {scheduled_time.isoformat()} (ваш часовой пояс)")
+
+        if delay > 0:
+            await asyncio.sleep(delay)  # Ждем до назначенного времени
+
+    # Логируем фактическое время отправки в вашем часовом поясе
+    logger.info(f"Отправка уведомления в {datetime.now(user_timezone).isoformat()} (ваш часовой пояс)")
     await reminder_news()
