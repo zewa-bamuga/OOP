5ad0ba22a72977cfc6bd62503d8a05caf9c0d94b
zewa-bamuga <tikhonov.igor2028@yandex.ru>
2024-11-13 01:46:43 +0700

    we are working on deleting reminders and closing the reminder task

--- src/app/domain/news/containers.py
+++ src/app/domain/news/containers.py
@@ -3,8 +3,8 @@ from dependency_injector import containers, providers
 from a8t_tools.db.transactions import AsyncDbTransaction
 
 from app.domain.news.commands import NewsCreateCommand, NewsPartialUpdateCommand, LikeTheNewsCommand, \
-    UnlikeTheNewsCommand, ReminderTheNewsCommand
-from app.domain.news.queries import NewsRetrieveQuery, NewsManagementListQuery, NewsListQuery
+    UnlikeTheNewsCommand, ReminderTheNewsCommand, DeleteReminderTheNewsCommand
+from app.domain.news.queries import NewsRetrieveQuery, NewsManagementListQuery, NewsListQuery, TaskNewsRetrieveQuery
 from app.domain.news.repositories import NewsRepository, LikeNewsRepository, ReminderNewsRepository
 from app.domain.users.containers import UserContainer
 
@@ -49,9 +49,24 @@ class NewsContainer(containers.DeclarativeContainer):
         query=news_list_query,
     )
 
+    task_news_query = providers.Factory(
+        TaskNewsRetrieveQuery,
+        reminder_news_repository=reminder_news_repository,
+    )
+
     reminder_the_news_command = providers.Factory(
         ReminderTheNewsCommand,
         news_retrieve_by_id_query=news_retrieve_by_id_query,
+        current_user_query=user_container.current_user_query,
+        news_repository=news_repository,
+        reminder_news_repository=reminder_news_repository,
+        task_producer=user_container.task_producer,
+    )
+
+    delete_reminder_the_news_command = providers.Factory(
+        DeleteReminderTheNewsCommand,
+        news_retrieve_by_id_query=news_retrieve_by_id_query,
+        task_news_query=task_news_query,
         current_user_query=user_container.current_user_query,
         news_repository=news_repository,
         reminder_news_repository=reminder_news_repository,
